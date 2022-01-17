7c31e517a296810600e0f91ca8753736e6c9c3c8
seois830926 <idmahoro@gmail.com>
2022-01-17 19:22:08 +0900

    CatDice객체 추가

---
+++ components/CatDice.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="cat-dice-container"></div>
+</template>
+
+<style>
+    div.cat-dice-container {
+        display: inline-block;
+    }
+</style>
+<script>
+export default {
+  name: 'CatDice',
+  data() {
+      return {
+          numberList: [1, 2, 3, 4, 5, 6]
+      }
+  },
+  methods: {
+      throwDice() {
+          return Math.floor(6 * Math.random()) + 1;
+      }
+  },
+  mounted() {
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,8 @@
       <cat-card v-bind:cat-count="this.numberList[0]" type="left"></cat-card>
       <cat-card v-bind:cat-count="this.numberList[1]" type="right"></cat-card>
     </div>
+    <div class="cat-dice-container"></div>
+    <button v-on:click="drawDice()">주사위던지기</button>
   </div>
 </template>
 
@@ -17,16 +19,19 @@
 </style>
 
 <script>
+import Vue from 'vue'
 import CatCard from '../components/CatCard.vue'
+import CatDice from '../components/CatDice.vue'
 import { mapMutations } from 'vuex'
 
 export default {
-  components: { CatCard },
+  components: { CatCard, CatDice },
   name: 'IndexPage',
   data() {
     return {
       drawCnt: 9,
-      numberList: []
+      numberList: [],
+      diceList: []
     }
   },
   computed: {
@@ -39,13 +44,28 @@ export default {
   },
   mounted() {
     this._setNumberList();
+    this._setDices();
   },
   methods: {
+    drawDice() {
+      this.diceList.forEach((dice) => {
+        const number = dice.throwDice();
+        console.log(number);
+      });
+    },
     _setNumberList() {
       this.numberList = [
         2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11
       ];
       this._shuffle();
+    },
+    _setDices() {
+      for(let i = 0; i < 4; i += 1) {
+        const catDice = Vue.extend(CatDice);
+        const cat_dice = new catDice();
+        cat_dice.$mount('.cat-dice-container');
+        this.diceList.push(cat_dice);
+      }
     },
     _shuffle() {
       this.numberList.sort(() => Math.random() - 0.5);
