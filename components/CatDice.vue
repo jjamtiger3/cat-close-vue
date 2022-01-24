f8403db656e1f4308a66902db7fddf9ae00397ed
seois830926 <idmahoro@gmail.com>
2022-01-24 19:46:38 +0900

    주사위 설계 및 css 설계중 커밋

--- components/CatDice.vue
+++ components/CatDice.vue
@@ -1,10 +1,20 @@
 <template>
-    <div class="cat-dice-container"></div>
+    <div class="cat-dice">
+        <div class="dice-face dice-face-front">1</div>
+        <div class="dice-face dice-face-back">2</div>
+        <div class="dice-face dice-face-right">3</div>
+        <div class="dice-face dice-face-left">4</div>
+        <div class="dice-face dice-face-top">5</div>
+        <div class="dice-face dice-face-bottom">6</div>
+    </div>
 </template>
 
 <style>
-    div.cat-dice-container {
-        display: inline-block;
+    div.cat-dice {
+        width: 100%;
+        height: 100%;
+        transform-style: preserve-3d;
+        transition: 1s;
     }
 </style>
 <script>
@@ -13,6 +23,8 @@ export default {
   data() {
       return {
           numberList: [1, 2, 3, 4, 5, 6],
+          displayedMarking: 1,
+          rolling: false,
           reSelected: false
       }
   },
--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,26 @@
       <cat-card v-bind:cat-count="this.numberList[0]" type="left"></cat-card>
       <cat-card v-bind:cat-count="this.numberList[1]" type="right"></cat-card>
     </div>
-    <div class="cat-dice-container"></div>
+    <div class="cat-dice-container">
+      <!-- 
+        주사위에 필요한 속성
+        1. displayedMarking: 표시할 주사위 눈금
+        2. rolling: 주사위가 굴러가는 상태
+        3. reselected: 다시 굴리기를 해야할 때 필요한 선택됨 상태
+        
+        주사위에 필요한 method
+        1. drawDice: 주사위를 던짐
+        2. reselect: 주사위를 다시 선택함
+
+        주사위를 위한 store가 필요할것같음
+        1. dices: Array<CatDice>: 주사위들을 저장한 객체
+        2. 각 dice에서 주사위의 속성상태를 제어함
+       -->
+      <cat-dice></cat-dice>
+      <cat-dice></cat-dice>
+      <cat-dice></cat-dice>
+      <cat-dice></cat-dice>
+    </div>
     <button v-on:click="drawDice()">주사위던지기</button>
   </div>
 </template>
@@ -48,6 +67,7 @@ export default {
   },
   methods: {
     drawDice() {
+      // 카드가 세팅됐을때만 주사위가 굴러가도록 해야함
       this.diceList.forEach((dice) => {
         const number = dice.throwDice();
         console.log(number);
